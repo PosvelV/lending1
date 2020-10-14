df2323b14a350fbfa27a3a208c1c69c96e828cb0
Vlapos <69690658+Vlapos@users.noreply.github.com>
2020-10-14 14:14:48 +0600

    Add files via upload

--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -1,6 +1,14 @@
 const buttonBurger = document.querySelector(".hamburger");
 const menu = document.querySelector(".fullscreen-menu");
 const close = document.querySelector(".close");
+const items = document.querySelectorAll(".navbar__link");
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", e => {
+    e.preventDefault();
+    menu.classList.remove("active");
+  });
+}
 
 buttonBurger.addEventListener("click", e => {
   e.preventDefault();
